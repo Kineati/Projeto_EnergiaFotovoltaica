8129b80529c399c0905774ab2568414672696eb6
Kineati <56892061+Kineati@users.noreply.github.com>
2022-02-08 17:41:34 -0300

    Update2 Projeto1.py

--- Projeto1.py
+++ Projeto1.py
@@ -4,6 +4,9 @@ def MediaDomiciliar_e_Fotovoltaica():
     EnergiaDomiciliarUsadaMes = []
     EnergiaDomiciliarPreçoMes = []
     aux5 = []
+    UnidadaFederativa = ''
+    Cidade = ''
+    País = ''
     ValorMedia_UsoDomiciliarMes = 0.0
     ValorMedia_PreçoDomiciliarMes = 0.0
     PreçoTotal_Ano = 0.0
@@ -12,6 +15,15 @@ def MediaDomiciliar_e_Fotovoltaica():
     MediaPorPessoa = 0.0
 
     TotalPessoas = int(input("Digite a Quantidade de Pessoas que Convivem na Mesma Casa:  "))
+    print("  ")
+
+    País = input("Digite o Pais que Voce reside:  ")
+    print("  ")
+
+    UnidadaFederativa = input("Digite a Região que voce reside:  ")
+    print("  ")
+
+    Cidade = input("Digite a Cidade que voce reside:  ")
     print("  ")
 
     for i in range(1,13):
@@ -37,11 +49,12 @@ def MediaDomiciliar_e_Fotovoltaica():
 
     def MediaFotovoltaica(ValorMedia_UsoDomiciliarMes,TotalPessoas,aux5):
         EnergiaFotovoltaicaProduz = {}
-        EnergiaFotovoltaicaConsume = []
-        EnergiaFotovoltaicaArmazena = []
+        EnergiaFotovoltaicaConsume = {}
+        EnergiaFotovoltaicaArmazena = {}
         EnergiaDomiciliareFotovoltaicaDif = []
         Cont_Painel = {}
         Cont_Baterias = {}
+        Cont_Anos = {}
         ValorMedia2 = 0.0
         ValorMedia3 = 0.0
         ValorMedia4 = 0.0
@@ -50,6 +63,7 @@ def MediaDomiciliar_e_Fotovoltaica():
         SomaTotal4 = 0.0
         TotalPaineisSolar = 0
         TotalBaterias = 0
+        TotalAnos = 0
 
         TotalPaineisSolar = int(input("Digite a Quantidade de Paines Solares que Voce Possui:  "))
         print("  ")
@@ -66,9 +80,12 @@ def MediaDomiciliar_e_Fotovoltaica():
                 SomaTotal2 += aux2
                 EnergiaFotovoltaicaProduz["Painel%s_Mes%s" %(i1,i)] = aux2
             Cont_Painel["Producao_Paineis"] = EnergiaFotovoltaicaProduz
+        
         ValorMedia2 = SomaTotal2/(TotalPaineisSolar*12)
+
         print("Media entre os paineis solares:  %f" % ValorMedia2)
         print("  ")
+
         print(Cont_Painel)
         print("  ")
         
@@ -78,19 +95,31 @@ def MediaDomiciliar_e_Fotovoltaica():
                 aux4 = float(input("Digite a Quantidade de energia Fotovoltaica (Killowatts) armazenada no mes %i (caso nao armazene,digite 0 em todos os meses), na bateria %i:   " %(i,i2)))
                 print("  ")
                 SomaTotal4 += aux4
-                EnergiaFotovoltaicaArmazena.insert(i,aux4)
+                EnergiaFotovoltaicaArmazena["Bateria%s_Mes%s" %(i2,i)] = aux4
             Cont_Baterias["Armazem_Bateria%s" %i2] = EnergiaFotovoltaicaArmazena
-            EnergiaFotovoltaicaArmazena.clear()
+            ValorMedia4 = SomaTotal4/(TotalBaterias*12)
+        
+        print("Media entre as baterias:  %f" % ValorMedia4)
+        print("  ")
+
         print(Cont_Baterias)
+        print("  ")
 
-        for i in range(1,13):
-            aux3 = float(input("Digite a Quantidade de energia Fotovoltaica (Killowatts) usada no mes %i (caso nao use,digite 0 em todos os meses): " % i))
-            print("  ")
-            SomaTotal3 += aux3
-            EnergiaFotovoltaicaConsume.insert(i,aux3)
-        ValorMedia3 = SomaTotal3/len(EnergiaFotovoltaicaConsume)
 
-        aux5.extend(EnergiaFotovoltaicaConsume)
+        for i3 in range(1,TotalAnos+1):
+            for i in range(1,13):
+                aux3 = float(input("Digite a Quantidade de energia Fotovoltaica (Killowatts) usada no mes %i (caso nao use,digite 0 em todos os meses): " % i))
+                print("  ")
+                SomaTotal3 += aux3
+                EnergiaFotovoltaicaConsume["Ano%s_Mes%s" % (i3,i)] = aux3
+            Cont_Anos["Consumo_EnergiaFotovoltaica%s" %i3] = EnergiaFotovoltaicaConsume
+            ValorMedia3 = SomaTotal3/(TotalAnos*12)
+
+        print("Media entre os anos  %f" % ValorMedia3)
+        print("  ")
+
+        print(Cont_Anos)
+        print("  ")
 
     MediaFotovoltaica(ValorMedia_UsoDomiciliarMes,TotalPessoas,aux5)
     
